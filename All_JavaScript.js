af1e73c0f5d896a44600cd9fc42113fe08204d1a
sachin-gavali <98352918+sachin-gavali@users.noreply.github.com>
2022-06-09 04:48:14 +0530

    Add files via upload

---
+++ All_JavaScript.js
@@ -0,0 +1,242 @@
+//console.log(" Sachin Gavali ");
+
+
+// console.log is use for printnting the output
+
+
+// Oprator 
+
+
+
+/* var num1 = 10;
+var num2 = 20;
+var sum = num1 + num2;
+var sub = num1 - num2;
+var multi = num1 * num2
+var div = num1 / num2;
+var mod = num1 % num2;
+console.log(" Num 1 is = " + num1);
+console.log(" Sum  is = " + sum);
+console.log(" Sub  is = " + sub);
+console.log(" multi  is = " + multi);
+console.log(" Div  is = " + div);
+console.log(" Mod   is = " + mod);
+console.log(" Num 2 is = " + num2); */
+
+
+
+
+// if else 
+
+
+
+/* let age = 18;
+if (age < 18) {
+    console.log(" You can't vote ");
+
+} else if (age == 18) {
+    console.log(" Sachin Gavali is best  ");
+
+} else {
+    console.log("You can vote ");
+
+} */
+
+
+
+
+// Switch case  in JavaScript 
+
+
+/* let roll_no = 17;
+switch (roll_no) {
+    case 10:
+        {
+            console.log(" Your numbe is 10");
+        }
+        break;
+    case 11:
+        {
+            console.log(" Your numbe is 11 ");
+        }
+        break;
+    case 12:
+        {
+            console.log(" Your numbe is 12");
+        }
+        break;
+    case 13:
+        {
+            console.log(" Your numbe is 13");
+        }
+        break;
+    case 14:
+        {
+            console.log(" Your numbe is 14");
+        }
+        break;
+    case 15:
+        {
+            console.log(" Your numbe is 15");
+        }
+        break;
+    case 16:
+        {
+            console.log(" Your numbe is 16");
+
+            break;
+        }
+    case 17:
+        {
+            console.log(" Your numbe is 17");
+        }
+        break;
+    case 18:
+        {
+            console.log(" Your numbe is18 ");
+        }
+        break;
+    case 19:
+        {
+            console.log(" Your numbe is 19");
+        }
+        break;
+    case 20:
+        {
+            console.log(" Your numbe is 20");
+        }
+        break;
+
+
+    default:
+        {
+            console.log(" Invald number");
+        }
+        break;
+} */
+
+
+
+// String function in  in JavaScript 
+
+
+/* let str = "Sachin Gavali";
+let str2 = " Hello ";
+console.log(str);
+console.log(str.length);
+console.log(str.indexOf(" Ga"))
+console.log(str.lastIndexOf(" in"));
+
+console.log(str.slice(1, 6));
+console.log(str.substring(1, 6));
+console.log(str.substring(1, 6));
+console.log(str + str2);
+console.log(str.concat(str2)); */
+// Object in JavaScript 
+/* let student = {
+    name: " Sachin Gavali ",
+    roll_no: 19,
+    phone_no: 9067339470,
+    fav_lang: " java",
+    fav_sub: " c programming ",
+};
+console.log(student); */
+
+
+
+// Array in JavaScript
+/* let names = [1, 2, 3];
+console.log(names);
+let Name = new Array(1, 2);
+console.log(Name);
+ */
+
+
+
+/* let names = [45, 46, 24, 23, 65, 76, 4, 34, 56, 3, 353, 35, 6];
+
+console.log(a.length);
+names = names.sort();
+console.log(names);
+names.push(" Sachin Gavali");
+console.log(names); */
+
+
+// Functions in JavaScript
+
+/* 
+let x = sum(10, 1);
+console.log(" The Sum is =" + x);
+let x1 = sum(10, 3);
+console.log(" The Sum is =" + x1);
+let x2 = sum(10, 5);
+console.log(" The Sum is =" + x2);
+let x3 = sum(10, 57);
+console.log(" The Sum is =" + x3);
+let x4 = sum(10, 54);
+console.log(" The Sum is =" + x4);
+let x5 = sum(157, 97);
+console.log(" The Sum is =" + x5);
+let x6 = sum(1568, 46);
+console.log(" The Sum is =" + x6);
+let x7 = sum(168, 956);
+console.log(" The Sum is =" + x7);
+let x8 = sum(196, 57);
+console.log(" The Sum is =" + x8);
+let x9 = sum(10, 68);
+console.log(" The Sum is =" + x9);
+
+
+
+function sum(a, b) {
+    return a + b;
+} */
+/* 
+let x = sum(10, 1);
+console.log(" The Multi is =" + x);
+let x1 = sum(10, 3);
+console.log(" The Multi is =" + x1);
+let x2 = sum(10, 5);
+console.log(" The Multi is =" + x2);
+let x3 = sum(10, 57);
+console.log(" The Multi is =" + x3);
+let x4 = sum(10, 54);
+console.log(" The Multi is =" + x4);
+let x5 = sum(157, 97);
+console.log(" The Multi is =" + x5);
+let x6 = sum(1568, 46);
+console.log(" The Multi is =" + x6);
+let x7 = sum(168, 956);
+console.log(" The Multi is =" + x7);
+let x8 = sum(196, 57);
+console.log(" The Multi is =" + x8);
+let x9 = sum(10, 68);
+console.log(" The Multi is =" + x9);
+
+
+
+function sum(a, b) {
+    return a * b;
+} */
+
+
+/* for loop in javaScript */
+/* let a = [10, 34, 53, 54, 64]
+for (let i = 0; i < a.length; i++) {
+    const Array_element = a[i];
+    console.log(Array_element);
+
+} */
+
+
+
+
+// Sum of Array 
+/* let a = [];
+
+let sum = 0;
+for (let i = 0; i < 5; i++) {
+    a[i] = prompt(" Enter the array element " + a + 1);
+    sum += a[i];
+    console.log(sum);
+} */
